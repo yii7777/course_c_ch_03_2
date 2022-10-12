5986f38aed6fd0044e29cb123eb49f6e48a8b8d6
yii7777 <115057596+yii7777@users.noreply.github.com>
2022-10-12 12:00:07 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	int num=0;
+	int space=0;
+	
+	while (num<5){
+		space=0;
+		while (space<=num){
+			printf ("*");
+		    space=space+1;
+		    
+	   }
+	    printf ("\n");
+		num=num+1;
+		} 
+	   return 0;
+	
+		} 
+
